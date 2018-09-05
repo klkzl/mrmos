9928e35573ab8f9c53e99e2f71045dccf0ecd64b
Koziel Klaudia <klaudia.koziel@gmail.com>
2018-09-05 14:50:22 +0200

    basic dependencies

---
+++ public/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Some default content from React.</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
